7d0a88a86d4c87fe361c97b75af95e374a5fb33d
TranMinhQuang888 <146643640+TranMinhQuang888@users.noreply.github.com>
2023-11-20 20:04:25 +0700

    Add files via upload

---
+++ home/DDOS/config/config.php
@@ -0,0 +1,31 @@
+<?php
+//EN - FR
+//CONFIGURATION - CONFIG
+//SITE NAME - Nom du site
+$sitename = 'LAYER7 - GRAPH';
+// CAPS ONLY - Majuscule seulement
+
+//Bit or byte - Bit ou octet
+// US=bit and/et EU= octet
+$zone = 'US';
+
+//Data transferred or received  -  Donnée transférée ou recus
+//RX = received/recus TX = transferred/transféerée
+$datatype = 'RX';
+
+//Language - Langue
+//FR = francais/french - EN = english/anglais
+$lang = 'EN';
+
+//Name of interface - Nom de l'interface
+//Usually : 'eth0' - Generalement :'eth0'
+$interface = 'eth0';
+
+// Ip of your server
+$ip = "https://dichvuright.cam/";
+$port = "443";
+
+//Graph title
+$Layer4Title = "Layer 4 ==>{$ip} PORT {$port} <==";
+$Layer7Title = "HIT ==> {$ip} PORT {$port} <==";
+
